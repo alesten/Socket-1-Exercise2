c643e7fd6696b37eeb81dcd6a8e2e81389fd57c3
Alexander Steen <AlexanderSteen@Alexanders-MacBook-Pro.local>
2015-08-27 11:24:04 +0200

    Exercise completed

--- src/socket/pkg1/exercise2/Socket1Exercise2.java
+++ src/socket/pkg1/exercise2/Socket1Exercise2.java
@@ -5,6 +5,12 @@
  */
 package socket.pkg1.exercise2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 /**
  *
  * @author AlexanderSteen
@@ -14,8 +20,15 @@ public class Socket1Exercise2 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        String ip = "localhost";
+        int port = 1234;
+        Socket link = new Socket(ip, port);
+        
+        PrintWriter out = new PrintWriter(link.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(link.getInputStream()));
+        
+        out.println();
+        System.out.println(in.readLine());
     }
-    
 }
